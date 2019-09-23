25f766de98b1978e5f143cf4f45fbfed0824163e
Sreekanth <sreekanth.nair@thoughtfocus.com>
2019-09-23 16:15:36 +0530

    added project structuree

---
+++ src/main/java/com/tf/emvparser/EmvparserApplication.java
@@ -0,0 +1,13 @@
+package com.tf.emvparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmvparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmvparserApplication.class, args);
+	}
+
+}
